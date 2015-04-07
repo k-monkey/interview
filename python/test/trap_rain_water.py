3a13e5e01b85900ee2d5b76f6408d57ba38e6bc8
k-monkey <farkeu1235@yahoo.com>
2015-04-07 16:55:08 -0700

    add solution for trap_rain_water

---
+++ python/test/trap_rain_water.py
@@ -0,0 +1,33 @@
+# Given n non-negative integers representing an elevation map where the width of each bar is 1, compute how much water it is able to trap after raining.
+
+# For example,
+# Given [0,1,0,2,1,0,1,3,2,1,2,1], return 6. 
+# see leetcode.com
+
+class Solution:
+    # @param A, a list of integers
+    # @return an integer
+    def trap(self, A):
+        if not A:
+            return 0
+            
+        biggest = -1
+        biggest_from_left = []
+        for i in A:
+            biggest_from_left.append(biggest)
+            biggest = max(i, biggest)
+        
+        biggest = -1
+        biggest_from_right = []
+        for i in reversed(A):
+            biggest_from_right.append(biggest)
+            biggest = max(i, biggest)
+        biggest_from_right.reverse()
+        
+        total_water = 0
+        for idx,element in enumerate(A):
+            elevation = min(biggest_from_left[idx], biggest_from_right[idx])
+            if element < elevation:
+                total_water += elevation - element
+        return total_water
+                
---
+++ python/test/trap_rain_water_test.py
@@ -0,0 +1,24 @@
+import unittest
+
+from trap_rain_water import Solution
+
+class MyTest(unittest.TestCase):
+    def test_A(self):
+        solution = Solution()
+        input1 = [0,1,0,2,1,0,1,3,2,1,2,1]
+        self.assertEqual(6, solution.trap(input1))
+        input2 = []
+        self.assertEqual(0, solution.trap(input2))       
+        input3 = [0,0,0]
+        self.assertEqual(0, solution.trap(input3))        
+        input4 = None
+        self.assertEqual(0, solution.trap(input4))   
+        input5 = [1,2,3,4,5,6]
+        self.assertEqual(0, solution.trap(input5))   
+        input6 = [6,5,4,3,2,1]
+        self.assertEqual(0, solution.trap(input6))   
+        
+if __name__ == '__main__':
+    fooSuite = unittest.TestLoader().loadTestsFromTestCase(MyTest)
+    fooRunner = unittest.TextTestRunner(descriptions=True)
+    fooRunner.run(fooSuite)
