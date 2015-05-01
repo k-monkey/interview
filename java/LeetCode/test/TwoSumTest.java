c5620bc62304d7fc8b0908a781a3979be11f41d5
k-monkey <farkeu1235@yahoo.com>
2015-05-01 12:40:35 -0700

    solution for TwoSum

---
+++ java/LeetCode/test/TwoSumTest.java
@@ -0,0 +1,18 @@
+import junit.framework.TestCase;
+
+import org.junit.Test;
+
+
+public class TwoSumTest extends TestCase {
+
+	@Test
+	public void testTwoSum() {
+		TwoSum ts = new TwoSum();
+		int[] a = {0,1,3,4,24,2,23};
+		int[] result = ts.twoSum(a,  25); 
+		assertEquals(2, result.length);
+		assertEquals(1, a[result[0]-1]);
+		assertEquals(24, a[result[1]-1]);		
+	}
+
+}
