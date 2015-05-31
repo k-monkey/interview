a59d9fec6eff40060a059e60454831f2411884bf
k-monkey <farkeu1235@yahoo.com>
2015-05-31 14:32:10 -0700

    used ExecutorService tool to manage threads

--- java/Others/src/threads/producer_consumer/ProducerConsumer.java
+++ java/Others/src/threads/producer_consumer/ProducerConsumer.java
@@ -1,25 +1,50 @@
 package threads.producer_consumer;
 
+import java.util.ArrayList;
 import java.util.concurrent.ArrayBlockingQueue;
 import java.util.concurrent.BlockingQueue;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.Future;
 
 public class ProducerConsumer {
     private ArrayBlockingQueue<Integer> bq;
     private int numProducers, numConsumers;
+    private ExecutorService consumerPool, producerPool;
     
     public ProducerConsumer(int capacity, int numProducers, int numConsumers) {
         bq = new ArrayBlockingQueue<Integer>(capacity);
+        this.consumerPool = Executors.newFixedThreadPool(numConsumers);
+        this.producerPool = Executors.newFixedThreadPool(numProducers);
         this.numConsumers = numConsumers;
         this.numProducers = numProducers;
     }
     
     public void run() {
+        ArrayList<Future<?>> futures = new ArrayList<Future<?>>();
         for (int i=0; i< numProducers; i++) {
-            (new Thread(new Producer(bq))).start();
+            futures.add(producerPool.submit(new Producer(bq)));
         }
         
         for (int i=0; i< numConsumers; i++) {
-            (new Thread(new Consumer(bq))).start();
+            futures.add(consumerPool.submit(new Consumer(bq)));
+        }
+        
+        System.out.println("futures: " + futures.size());
+        for (Future<?> f : futures) {
+            try {
+                f.get();
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+                System.out.println("one thread failed");
+                continue;
+            } catch (ExecutionException e) {
+                System.out.println("one thread failed");
+                e.printStackTrace();
+                continue;
+            }
+            System.out.println("one thread finished");
         }
     }
     
@@ -36,7 +61,6 @@ public class ProducerConsumer {
                 try {
                     bqueue.put(new Integer((int) Math.round(Math.random()*1000000)));
                 } catch (InterruptedException e) {
-                    // TODO Auto-generated catch block
                     e.printStackTrace();
                 }
             }
@@ -54,7 +78,7 @@ public class ProducerConsumer {
         public void run() {
             while (true) {
                 try {
-                    System.out.println("consumed: " + bq.take());
+                    System.out.println("consumed: " + bqueue.take());
                 } catch (InterruptedException e) {
                     // TODO Auto-generated catch block
                     e.printStackTrace();
--- java/Others/test/threads/producer_consumer/ProducerConsumerTest.java
+++ java/Others/test/threads/producer_consumer/ProducerConsumerTest.java
@@ -1,13 +1,13 @@
 package threads.producer_consumer;
 
-import static org.junit.Assert.*;
-
 import org.junit.Test;
 
 public class ProducerConsumerTest {
     @Test
     public void testRun() {
-        (new ProducerConsumer(10, 10, 2)).run();
-        fail("test finished");
+        //int count = 0;
+        //while (count++ < 10) {
+            (new ProducerConsumer(10, 1, 1)).run();
+        //}
     }
 }
