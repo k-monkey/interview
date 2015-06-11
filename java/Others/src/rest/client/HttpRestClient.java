d8abf3af27af97c6b732b26ed2cbe9fdce4745c5
k-monkey <farkeu1235@yahoo.com>
2015-06-11 12:16:16 -0700

    add restful client implementation

--- java/Others/src/rest/client/HttpRestClient.java
+++ java/Others/src/rest/client/HttpRestClient.java
@@ -1,38 +1,43 @@
 package rest.client;
 
+import org.apache.http.Header;
+import org.apache.http.HttpEntity;
+import org.apache.http.HttpHost;
+import org.apache.http.HttpResponse;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.impl.client.CloseableHttpClient;
+import org.apache.http.impl.client.HttpClientBuilder;
+import org.apache.http.util.EntityUtils;
+
 public class HttpRestClient {
-//    DefaultHttpClient httpclient = new DefaultHttpClient();
-//    try {
-//      // specify the host, protocol, and port
-//      HttpHost target = new HttpHost("weather.yahooapis.com", 80, "http");
-//      
-//      // specify the get request
-//      HttpGet getRequest = new HttpGet("/forecastrss?p=80020&u=f");
-//
-//      System.out.println("executing request to " + target);
-//
-//      HttpResponse httpResponse = httpclient.execute(target, getRequest);
-//      HttpEntity entity = httpResponse.getEntity();
-//
-//      System.out.println("----------------------------------------");
-//      System.out.println(httpResponse.getStatusLine());
-//      Header[] headers = httpResponse.getAllHeaders();
-//      for (int i = 0; i < headers.length; i++) {
-//        System.out.println(headers[i]);
-//      }
-//      System.out.println("----------------------------------------");
-//
-//      if (entity != null) {
-//        System.out.println(EntityUtils.toString(entity));
-//      }
-//
-//    } catch (Exception e) {
-//      e.printStackTrace();
-//    } finally {
-//      // When HttpClient instance is no longer needed,
-//      // shut down the connection manager to ensure
-//      // immediate deallocation of all system resources
-//      httpclient.getConnectionManager().shutdown();
-//    }
-//  }
+
+    public static void runClient() {
+        try (CloseableHttpClient httpclient = HttpClientBuilder.create().build()) {
+            // specify the host, protocol, and port
+            HttpHost target = new HttpHost("weather.yahooapis.com", 80, "http");
+
+            // specify the get request
+            HttpGet getRequest = new HttpGet("/forecastrss?p=80020&u=f");
+
+            System.out.println("executing request to " + target);
+
+            HttpResponse httpResponse = httpclient.execute(target, getRequest);
+            HttpEntity entity = httpResponse.getEntity();
+
+            System.out.println("----------------------------------------");
+            System.out.println(httpResponse.getStatusLine());
+            Header[] headers = httpResponse.getAllHeaders();
+            for (int i = 0; i < headers.length; i++) {
+                System.out.println(headers[i]);
+            }
+            System.out.println("----------------------------------------");
+
+            if (entity != null) {
+                System.out.println(EntityUtils.toString(entity));
+            }
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
 }
---
+++ java/Others/test/rest/client/HttpRestClientTest.java
@@ -0,0 +1,15 @@
+package rest.client;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class HttpRestClientTest {
+
+    @Test
+    public void testRunClient() {
+        HttpRestClient.runClient();
+        fail("Not yet implemented");
+    }
+
+}
